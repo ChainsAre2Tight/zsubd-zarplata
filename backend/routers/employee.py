a49befa6ce15691b6642eda67a7dd7a711575d9c
Dmitry <dima124691@gmail.com>
2024-10-14 11:54:22 +0300

    updates create employee with database connection dependency and adds user inserting

--- backend/routers/employee.py
+++ backend/routers/employee.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter
 
 import backend.models.employee as models
-
+from backend.dependencies import get_db_cursor
 
 router = APIRouter(prefix='/employee')
 
 
 @router.post('', status_code=201, response_model=models.EmployeeResponse)
-def create_employee(data: models.EmployeeToCreate):
-    return {'employee_uuid': 'test'}
+def create_employee(
+        cursor: get_db_cursor,
+        data: models.EmployeeToCreate,
+    ):
+    cursor.execute('insert into employee(fio) VALUES (%s) RETURNING id', (data.fio, ))
+    id = cursor.fetchone()[0]
+    return {'employee_uuid': id}
