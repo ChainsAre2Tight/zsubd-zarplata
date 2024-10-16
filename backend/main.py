5460eeb96b0d4f72a315c5f4e0fa589a24a8a8a9
Dmitry <dima124691@gmail.com>
2024-10-16 13:46:24 +0300

    adds CORS policy

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 import backend.routers.employee as employee
 
 
 app = FastAPI()
+
+origins = [
+    "http://zarplata.zsubd",
+    "http://localhost:5000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 base_router = APIRouter(prefix='/api/v1')
 base_router.include_router(employee.router)
