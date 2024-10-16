411a3017f4c0087d2cc3e092688d7cddb0a6bcf2
Dmitry <dima124691@gmail.com>
2024-10-16 18:28:30 +0300

    get_current_user now return employee uuid

--- backend/auth.py
+++ backend/auth.py
@@ -1,10 +1,10 @@
 from fastapi.security import OAuth2PasswordBearer
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from passlib.context import CryptContext
-from datetime import datetime, timedelta, timezone
 import jwt
 
 from typing import Annotated
+from datetime import datetime, timedelta, timezone
 
 from backend.models.employee import EmployeeUser
 
@@ -15,20 +15,25 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 _oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 _token = Annotated[str, Depends(_oauth2_scheme)]
 
-def _fake_decode_token(token: str) -> EmployeeUser:
-    return EmployeeUser(
-        uuid=token,
-    )
-
-def decode_token(token: str) -> EmployeeUser:
+def decode_token(token: str) -> dict:
     return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
 def _get_current_user(token: _token) -> EmployeeUser:
-    return _fake_decode_token(token=token)
-
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        data = decode_token(token)
+    except jwt.InvalidTokenError:
+        raise credentials_exception
+    # if 'employee' not in data.get('scope')
+    return EmployeeUser(
+        uuid=data['employeeUUID'],
+    )
 
 get_current_user = Annotated[EmployeeUser, Depends(_get_current_user)]
-
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain_password, hashed_password):
