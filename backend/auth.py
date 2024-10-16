d379fc39eb48ffc27c20b4bf7c62db62216da682
Dmitry <dima124691@gmail.com>
2024-10-16 17:43:11 +0300

    adds some of password flow

--- backend/auth.py
+++ backend/auth.py
@@ -1,8 +1,16 @@
 from fastapi.security import OAuth2PasswordBearer
 from fastapi import Depends
+from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+import jwt
+
 from typing import Annotated
 
 from backend.models.employee import EmployeeUser
+
+SECRET_KEY = 'aboba'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 _oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 _token = Annotated[str, Depends(_oauth2_scheme)]
@@ -17,3 +25,21 @@ def _get_current_user(token: _token) -> EmployeeUser:
 
 
 get_current_user = Annotated[EmployeeUser, Depends(_get_current_user)]
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -3,10 +3,38 @@ from fastapi.security import OAuth2PasswordRequestForm
 
 from typing import Annotated
 
+from backend.dependencies import get_db_cursor
+from backend.auth import get_password_hash, create_access_token
+from backend.models.auth import Token
+
 router = APIRouter(prefix='/token')
 
-@router.post('')
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    if not (form_data.username == 'test' and form_data.password == 'mypwd'):
-        raise HTTPException(status_code=400, detail='Incorrect user or password')
-    return {'access_token': 'test-token', 'token_type': 'bearer'}
+@router.post('', response_model=Token)
+async def login(
+        cursor: get_db_cursor,
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+    ):
+
+    def verify_user(cursor, username, hashed_pwd) -> tuple | None:
+        print('...', username, hashed_pwd)
+        cursor.execute(
+            "SELECT employee_id FROM employee_user WHERE username = %s AND pwd = %s LIMIT 1",
+            (username, hashed_pwd)
+        )
+        result = cursor.fetchone()
+        return result
+
+    employee_id = verify_user(
+        cursor=cursor,
+        username=form_data.username,
+        hashed_pwd=get_password_hash(form_data.password)
+    )
+    print(employee_id)
+    if employee_id is None:
+        raise HTTPException(status_code=403, detail='Invalid username or password')
+    
+    access_token = create_access_token(
+        data={'employeeUUID': employee_id, 'scope': 'employee'}
+    )
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
