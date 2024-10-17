fabc4decaedb931d7c4552e07ac1bb8fb440216a
Dmitry <dima124691@gmail.com>
2024-10-17 15:16:34 +0300

    adds vacation model

---
+++ backend/models/vacation.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, field_validator
+
+from datetime import datetime
+
+
+def validate_date_format(date: str) -> bool:
+    try:
+        datetime.fromisoformat(date)
+    except:
+        return False
+    return True
+
+
+class VacationIn(BaseModel):
+    begin_date: str
+    end_date: str
+
+    @field_validator('begin_date', 'end_date')
+    def validate_dates(cls, v: str):
+        if not validate_date_format(v):
+            raise ValueError('Date is not in ISO8601 format')
