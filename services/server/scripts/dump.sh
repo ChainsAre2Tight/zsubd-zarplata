3a858f3699967d95b482b9e86a9f952be3c1ce1e
ChainsAre2Tight <dima124691@gmai9l.com>
2024-12-13 08:01:22 +0300

    adds dump script

---
+++ services/server/scripts/dump.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set +x
+set -e
+
+database=zarplata
+directory=/usr/src/backups
+max_backups=2
+
+echo "Starting..."
+
+echo Scanning backup directory at ${directory}...
+filecount="$(find ${directory} -type f | wc -l)"
+
+if [[ $filecount -gt $(($max_backups - 1)) ]]
+then
+    echo There are already ${filecount} backups present '('max ${max_backups}')'
+
+    file_to_delete="$(ls -rt ${directory} | head -n 1)"
+    echo Deleting the oldest backup '('${file_to_delete}')'
+    rm -- ${directory}/${file_to_delete}
+    
+    echo Deletion successful
+else
+    echo There is enough space to create a backup
+fi
+
+
+
+echo "Starting backup..."
+filename=dump-${database}-"`date +"%d-%m-%Y_%H-%M-%S"`"
+
+echo Saving "${database}" to ${filename}
+pg_dump ${database} > ${directory}/${filename}
+
+echo "backup successful"
